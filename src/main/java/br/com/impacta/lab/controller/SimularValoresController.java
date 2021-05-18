82a96e91f78c0cab6e4f6ba584a858afec27599c
Natalia Milani <natalia.milani@hotmail.com>
2021-05-18 00:04:21 -0300

    Desafio realizado

--- src/main/java/br/com/impacta/lab/controller/SimularValoresController.java
+++ src/main/java/br/com/impacta/lab/controller/SimularValoresController.java
@@ -1,5 +1,7 @@
 package br.com.impacta.lab.controller;
 
+import java.text.DecimalFormat;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,8 +53,119 @@ public class SimularValoresController {
 		 * 
 		 */
 		
+		String desc_produto = "", desc_cond_pagamento = "";
+		Double valor_produto = 0.00, valor_prod_desconto = 0.00;
+		DecimalFormat df = new DecimalFormat("#0.00");
+		Boolean retorno = true;
+
+		if (codigoProduto == 1){
+			desc_produto = "Camisa";
+			valor_produto = 70.00;
+
+			if (codTipoPagamento == 1){
+				desc_cond_pagamento = "A vista no dinheiro com 10% de desconto";
+				valor_produto = (valor_produto - (10.00/100.00 * valor_produto));
+			}else if (codTipoPagamento == 2){
+				desc_cond_pagamento = "A vista no cartão de crédito com 5% de desconto";
+				valor_produto = (valor_produto - (5.00/100.00 * valor_produto));				
+			}else if (codTipoPagamento == 3){
+				desc_cond_pagamento = "Em duas parcelas sem nenhum desconto";
+				valor_produto = (valor_produto / 2);
+			}else if (codTipoPagamento == 4){
+				desc_cond_pagamento = "Em três vezes com 10% de juros";
+				valor_produto = ((valor_produto - (10.00/100.00 * valor_produto)) /3);				
+			}else{
+				retorno = false;
+			}			
 		
-		return ResponseEntity.ok("Hello world !");
+		} else if(codigoProduto == 2){
+			desc_produto = "Shorts";
+			valor_produto = 57.50;
+
+			if (codTipoPagamento == 1){
+				desc_cond_pagamento = "A vista no dinheiro com 10% de desconto";
+				valor_produto = (valor_produto - (10.00/100.00 * valor_produto));
+			}else if (codTipoPagamento == 2){
+				desc_cond_pagamento = "A vista no cartão de crédito com 5% de desconto";
+				valor_produto = (valor_produto - (5.00/100.00 * valor_produto));				
+			}else if (codTipoPagamento == 3){
+				desc_cond_pagamento = "Em duas parcelas sem nenhum desconto";
+				valor_produto = (valor_produto / 2);
+			}else if (codTipoPagamento == 4){
+				desc_cond_pagamento = "Em três vezes com 10% de juros";
+				valor_produto = ((valor_produto - (10.00/100.00 * valor_produto)) /3);				
+			}else{
+				retorno = false;
+			}
+
+		} else if(codigoProduto == 3){
+			desc_produto = "Meia";
+			valor_produto = 9.99;
+
+			if (codTipoPagamento == 1){
+				desc_cond_pagamento = "A vista no dinheiro com 10% de desconto";
+				valor_produto = (valor_produto - (10.00/100.00 * valor_produto));
+			}else if (codTipoPagamento == 2){
+				desc_cond_pagamento = "A vista no cartão de crédito com 5% de desconto";
+				valor_produto = (valor_produto - (5.00/100.00 * valor_produto));				
+			}else if (codTipoPagamento == 3){
+				desc_cond_pagamento = "Em duas parcelas sem nenhum desconto";
+				valor_produto = (valor_produto / 2);
+			}else if (codTipoPagamento == 4){
+				desc_cond_pagamento = "Em três vezes com 10% de juros";
+				valor_produto = ((valor_produto - (10.00/100.00 * valor_produto)) /3);				
+			}else{
+				retorno = false;
+			}			
+		} else if(codigoProduto == 4){
+			desc_produto = "Touca";
+			valor_produto = 35.00;
+
+			if (codTipoPagamento == 1){
+				desc_cond_pagamento = "A vista no dinheiro com 10% de desconto";
+				valor_produto = (valor_produto - (10.00/100.00 * valor_produto));
+			}else if (codTipoPagamento == 2){
+				desc_cond_pagamento = "A vista no cartão de crédito com 5% de desconto";
+				valor_produto = (valor_produto - (5.00/100.00 * valor_produto));				
+			}else if (codTipoPagamento == 3){
+				desc_cond_pagamento = "Em duas parcelas sem nenhum desconto";
+				valor_produto = (valor_produto / 2);
+			}else if (codTipoPagamento == 4){
+				desc_cond_pagamento = "Em três vezes com 10% de juros";
+				valor_produto = ((valor_produto - (10.00/100.00 * valor_produto)) /3);				
+			}else{
+				retorno = false;
+			}			
+		} else if(codigoProduto == 5){
+			desc_produto = "Luvas";
+			valor_produto = 19.50;
+
+			if (codTipoPagamento == 1){
+				desc_cond_pagamento = "A vista no dinheiro com 10% de desconto";
+				valor_produto = (valor_produto - (10.00/100.00 * valor_produto));
+			}else if (codTipoPagamento == 2){
+				desc_cond_pagamento = "A vista no cartão de crédito com 5% de desconto";
+				valor_produto = (valor_produto - (5.00/100.00 * valor_produto));				
+			}else if (codTipoPagamento == 3){
+				desc_cond_pagamento = "Em duas parcelas sem nenhum desconto";
+				valor_produto = (valor_produto / 2);
+			}else if (codTipoPagamento == 4){
+				desc_cond_pagamento = "Em três vezes com 10% de juros";
+				valor_produto = ((valor_produto - (10.00/100.00 * valor_produto)) /3);				
+			}else{
+				retorno = false;
+			}			
+		} else{
+			retorno = false;
+		}
+
+
+		if (retorno){
+			return ResponseEntity.ok(desc_produto + " sendo pago " + desc_cond_pagamento + " custará " + df.format(valor_produto));
+		}else{
+			return ResponseEntity.ok("Parametro informado não existe, favor informar código de produto entre 1 e 5, e forma de pagamento entre 1 e 4");
+		}
+		
 	}
 	
 }
